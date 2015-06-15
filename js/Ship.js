95bd54674e33b423d7198cbfb0de4428f774900d
Max Nijholt <m.nijholt@student.avans.nl>
2015-06-15 12:19:57 +0200

    GetShips toegevoegd.

--- js/Ship.js
+++ js/Ship.js
@@ -2,3 +2,17 @@
  * Created by toinebakkeren on 09/06/15.
  */
 
+function ShipsModel() {
+
+    this.getShipsFromAPI = function(callback) {
+        $.ajax({
+            url:    'https://zeeslagavans.herokuapp.com/ships?token='
+            + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.ImFtYmFra2VyMUBhdmFucy5ubCI.opl6vYsBdYBruG9cnZz2P_JnN4b60T7XOjs4-DmZbYk",
+            success: function(result) {
+                callback(result);
+            },
+            dataType: "json"
+        });
+    }
+    
+}
